749338a04d30eed8674c0c6e4e6d78e2eb4dbf68
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-21 17:12:27 +0300

    Finalizat fara TODO.

--- Conversie/Program.cs
+++ Conversie/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,37 @@ namespace Conversie
     {
         static void Main(string[] args)
         {
+            // Cornertiti un numar din baza 10 intr-o baza tinta
+            string line;
+
+            Console.WriteLine("Introduceti numarul pe care doriti sa-l convertiti");
+            line = Console.ReadLine();
+            int numar = int.Parse(line);
+
+            Console.WriteLine("Introduceti baza tinta, nr. intre 2-16");
+            line = Console.ReadLine();
+            int bazaTinta = int.Parse(line);
+
+            int cat, rest;
+            string result = "";
+            Stack<int> stiva = new Stack<int>();
+
+            while (numar > 0)
+            {
+                cat = numar / bazaTinta;
+                rest = numar % bazaTinta;
+                stiva.Push(rest);
+                numar = numar / bazaTinta;
+
+            }
+
+            while (stiva.Count > 0)
+            {
+                result = result + stiva.Pop();
+
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
