331ebae435d54646e6d50892a82305eb3890971f
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-21 16:40:27 +0300

    Am finalizat exercitiul.

---
+++ Ghicește numărul/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghicește_numărul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se scrie un program în care calculatorul generează un numar orareceare, iar persoana din fața
+            //calculatorului trebuie să gjiceasca numarul.
+
+            Random x = new Random();
+
+            int s = x.Next(100);
+            int a;
+            Console.WriteLine("Introduceti un numar intreg intre 0-100");
+            do
+            {
+                
+                a = int.Parse(Console.ReadLine());
+                if (a < s)
+                    Console.WriteLine("Incearca mai mare");
+                else if (a == s)
+                {
+                    Console.WriteLine("Ai ghicit");
+                }
+                else 
+                        Console.WriteLine("Incearca mai mic");
+            }
+            while (a != s);
+            Console.ReadKey();
+
+
+        }
+    }
+}
